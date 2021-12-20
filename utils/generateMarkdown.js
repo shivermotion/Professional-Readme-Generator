5e71565b1833de0215f9e24a0022c1f8908f1cef
Jason Day <jason.day.blue@gmail.com>
2021-12-20 00:01:46 -0600

    fixing license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,28 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-	if (license === "MIT") {
-		return "[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)";
+function renderLicenseBadge(response) {
+	if (`${response.license}` === "MIT") {
+		return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
 	}
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-	if (license === "Apache 2.0") {
-		return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-	}
-}
+function renderLicenseLink(response) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-	if (license === "") {
-		return;
-	}
-}
+function renderLicenseSection(response) {}
 
-// // TODO: Create a function to generate markdown for README
+// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
 	return `# ${data.title}
 
