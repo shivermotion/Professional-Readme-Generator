f65fb2a93841cb3d50c4c21aa4b13c03e113327c
Jason Day <jason.day.blue@gmail.com>
2021-12-21 00:10:33 -0600

    save

--- README.md
+++ README.md
@@ -1,33 +1,31 @@
-# Employee-Tracker
+
+# e-commerce backend
 
 # Table of Content
-
--[description](#description) -[installation](#installation) -[usage](#usage) -[licenses](#licenses) -[contribution](#contribution) -[test](#test) -[username](#username) -[profile](#profile)
+-[description](#description)
+-[installation](#installation)
+-[usage](#usage)
+-[licenses](#licenses)
+-[contribution](#contribution)
+-[test](#test)
+-[username](#username)
+-[profile](#profile)
 
 ## Username:
-
 shivermotion
-
 ## Description :
-
-a command-line application from scratch to manage a company's employee database, using Node.js, Inquirer, and MySQL.
-
+respresentation of an ecommerce backend
 ## Installation :
 
 ## Usage :
 
 ## Licenses :
-
 MIT
-
 ## Contribution:
 
 ## Test:
 
 ## Email:
-
 jason.day.blue@gmail.com
-
 ## Profile:
-
 https://github.com/shivermotion
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,25 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(response) {
-	if (`${response.license}` === "MIT") {
-		return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-	}
-}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(response) {
+// 	if (`${response.license}` === "MIT") {
+// 		return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+// 	}
+// }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(response) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(response) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(response) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(response) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-	return `# ${data.title}
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+// 	return `# ${data.title}
 
-`;
-}
+// `;
+// }
 
 module.exports = generateMarkdown;
 
@@ -46,7 +46,7 @@ ${response.installation}
 ## Usage :
 ${response.usage}
 ## Licenses :
-${renderLicenseBadge()}
+${response.license}
 ## Contribution:
 ${response.guidelines}
 ## Test:
