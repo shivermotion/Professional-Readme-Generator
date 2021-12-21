003b000950f807517447a63f04605bff08336d23
Jason Day <jason.day.blue@gmail.com>
2021-12-21 01:15:09 -0600

    license badge fix

--- README.md
+++ README.md
@@ -1,37 +1,36 @@
 
-# e-commerce backend
+	# readme-gen
+	
+	# Table of Content
+	-[description](#description)
+	-[installation](#installation)
+	-[usage](#usage)
+	-[licenses](#licenses)
+	-[contribution](#contribution)
+	-[test](#test)
+	-[username](#username)
+	-[profile](#profile)
+	
+	## Username:
+	shivermotion
+	## Description :
+	readme generator
+	## Installation :
+	
+	## Usage :
+	
 
-# Table of Content
--[description](#description)
--[installation](#installation)
--[usage](#usage)
--[licenses](#licenses)
--[contribution](#contribution)
--[test](#test)
--[username](#username)
--[profile](#profile)
+	## Licenses :
+		[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+		https://opensource.org/licenses/MIT
+		
 
-## Username:
-shivermotion
-## Description :
-respresentation of an ecommerce backend
-## Installation :
-
-## Usage :
-
-## Licenses :
-<<<<<<< HEAD
-MIT
-=======
-
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-
->>>>>>> f0b77e8370d06cf7ec6f8fc63155083f03df09de
-## Contribution:
-
-## Test:
-
-## Email:
-jason.day.blue@gmail.com
-## Profile:
-https://github.com/shivermotion
+	## Contribution:
+	
+	## Test:
+	
+	## Email:
+	jason.day.blue@gmail.com
+	## Profile:
+	github.com/shivermotion
+	
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,59 +1,80 @@
-// // TODO: Create a function that returns a license badge based on which license is passed in
-// // If there is no license, return an empty string
-// function renderLicenseBadge(response) {
-// 	if (`${response.license}` === "MIT") {
-// 		return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-// 	}
-// }
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(response) {
+	console.log(response);
+	if (response.license === "MIT") {
+		return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+	} else if (response.license === "Apache 2.0") {
+		return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+	} else if (response.license === "GNU") {
+		return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+	} else if (response.license === "none") {
+		return "";
+	}
+}
 
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(response) {}
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(response) {
+	if (response.license === "MIT") {
+		return "https://opensource.org/licenses/MIT";
+	} else if (response.license === "Apache 2.0") {
+		return "https://opensource.org/licenses/Apache-2.0";
+	} else if (response.license === "GNU") {
+		return "https://www.gnu.org/licenses/gpl-3.0";
+	} else if (response.license === "none") {
+		return "";
+	}
+}
 
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(response) {}
-
-// // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-// 	return `# ${data.title}
-
-// `;
-// }
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(response) {
+	if (response.license === "MIT" || "GNU" || "Apache 2.0") {
+		return `## Licenses :
+		${renderLicenseBadge(response)}
+		${renderLicenseLink(response)}
+		`;
+	} else {
+		return "";
+	}
+}
 
 module.exports = generateMarkdown;
+// TODO: Create a function to generate markdown for README
 
 function generateMarkdown(response) {
 	return `
-# ${response.title}
+	# ${response.title}
+	
+	# Table of Content
+	-[description](#description)
+	-[installation](#installation)
+	-[usage](#usage)
+	-[licenses](#licenses)
+	-[contribution](#contribution)
+	-[test](#test)
+	-[username](#username)
+	-[profile](#profile)
+	
+	## Username:
+	${response.username}
+	## Description :
+	${response.description}
+	## Installation :
+	${response.installation}
+	## Usage :
+	${response.usage}
 
-# Table of Content
--[description](#description)
--[installation](#installation)
--[usage](#usage)
--[licenses](#licenses)
--[contribution](#contribution)
--[test](#test)
--[username](#username)
--[profile](#profile)
+	${renderLicenseSection(response)}
 
-## Username:
-${response.username}
-## Description :
-${response.description}
-## Installation :
-${response.installation}
-## Usage :
-${response.usage}
-## Licenses :
-${response.license}
-## Contribution:
-${response.guidelines}
-## Test:
-${response.test}
-## Email:
-${response.email}
-## Profile:
-${response.profile}
-`;
+	## Contribution:
+	${response.guidelines}
+	## Test:
+	${response.test}
+	## Email:
+	${response.email}
+	## Profile:
+	${response.profile}
+	`;
 }
